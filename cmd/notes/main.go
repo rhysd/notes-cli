55a02c4688d3d23329a8df2223cef06312f1dddd
rhysd <lin90162@yahoo.co.jp>
2018-11-25 09:10:38 +0900

    use red string for error message header

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/mattn/go-colorable"
 	"github.com/rhysd/notes-cli"
 	"os"
 )
 
 func exit(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "notes: error: %s\n", err.Error())
+		fmt.Fprintln(colorable.NewColorableStderr(), color.RedString("notes: error:"), err.Error())
 		os.Exit(110)
 	}
 	os.Exit(0)
